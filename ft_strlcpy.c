564c5a64b116e19763a87f6d68cd1881292c19f8
tramet <theo.ramet@gmail.com>
2019-01-10 15:36:04 +0100

    compilation_ok

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -12,16 +12,16 @@
 
 #include "libft.h"
 
-size_t		strlcpy(char *dst, const char *src, size_t size)
+size_t		ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	unsigned int	i;
 
 	i = 0;
 	while (--size && *(src + i))
 	{
-		*(dest + i) = *(src + i);
+		*(dst + i) = *(src + i);
 		i++;
 	}
-	*(dest + i) = '\0';
+	*(dst + i) = '\0';
 	return (ft_strlen(src));
 }
